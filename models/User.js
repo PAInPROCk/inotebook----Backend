091be96c04eedc47938bd3cb17b37e9435a6b192
PAInPROCk <2601prathmeshbhadwalkar@gmail.com>
2025-04-22 14:06:22 +0530

    2:05 22-04-2025 CWH v46 R 3:38

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose; // 👈
 
+
+
 const UserSchema = new Schema({
     name: {
         type: String,
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User'); 
+const { body, validationResult } = require('express-validator');
 
-router.post('/', async (req, res) => {  
+router.post('/', [
+  body('name').isLength({ min: 3 }),
+  body('email').isEmail(),
+  body('password').isLength({ min: 8 }),
+], async (req, res) => {  
+  const errors = validationResult(req);
+  
+  // Fix variable name from 'error' to 'errors'
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });  // Fix 'set.status' to 'res.status'
+  }
+
   try {
     console.log(req.body);
     const user = new User(req.body);  
