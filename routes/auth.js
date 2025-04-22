b77590e0289a9cafb47c4fba02cad94ec728f8fb
PAInPROCk <2601prathmeshbhadwalkar@gmail.com>
2025-04-22 15:21:04 +0530

    3:20 22-04-2025 CWH v46 C

--- routes/auth.js
+++ routes/auth.js
@@ -4,22 +4,24 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
 router.post('/', [
-  body('name').isLength({ min: 3 }),
-  body('email').isEmail(),
-  body('password').isLength({ min: 8 }),
+  body('name', 'Enter a Valid Name').isLength({ min: 3 }),
+  body('email', 'Enter a Valid Email').isEmail(),
+  body('password', 'Password must be at least 8 characters').isLength({ min: 8 }),
 ], async (req, res) => {  
   const errors = validationResult(req);
-  
-  // Fix variable name from 'error' to 'errors'
+
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });  // Fix 'set.status' to 'res.status'
+    return res.status(400).json({ errors: errors.array() });
   }
 
   try {
-    console.log(req.body);
-    const user = new User(req.body);  
-    await user.save();                
-    res.status(201).send(user);       
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    res.json(user);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
